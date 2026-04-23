1d32b0cd532b406267b1f8f6b5cbb29d4305eea9
David Arriaga <david.arriaga@formos.com>
2026-04-23 10:00:01 -0500

    Add models

--- client/src/main/java/com/app/client/model/Client.java
+++ client/src/main/java/com/app/client/model/Client.java
@@ -1,4 +1,18 @@
 package com.app.client.model;
 
-public class Client {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "client")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public class Client extends Person {
+    private String clientId;
+    private String password;
+    private Boolean status;
 }
--- client/src/main/java/com/app/client/model/Person.java
+++ client/src/main/java/com/app/client/model/Person.java
@@ -1,4 +1,23 @@
 package com.app.client.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@MappedSuperclass
+@Data
+@NoArgsConstructor
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String gender;
+    private short age;
+    private String identification;
+    private String address;
+    private String phone;
 }
