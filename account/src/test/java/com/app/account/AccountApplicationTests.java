753530de747fe05c3ebd2ef777785b136e7e9fcb
David Arriaga <david.arriaga@formos.com>
2026-04-23 16:46:49 -0500

    Add tests

--- account/src/test/java/com/app/account/AccountApplicationTests.java
+++ account/src/test/java/com/app/account/AccountApplicationTests.java
@@ -1,13 +1,75 @@
 package com.app.account;
 
+import com.app.account.controller.AccountController;
+import com.app.account.model.dto.AccountDto;
+import com.app.account.model.dto.CreateAccountDto;
+import com.app.account.model.enumeration.AccountType;
+import com.app.account.service.AccountService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class AccountApplicationTests {
+    private final AccountService accountService = mock(AccountService.class);
+    private final AccountController accountController = new AccountController(accountService);
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+
+    @Test
+    void saveAccountTest() {
+        CreateAccountDto newAccount = new CreateAccountDto(
+                "12345678",
+                1L,
+                AccountType.CHECKING,
+                BigDecimal.valueOf(2000.0)
+        );
+        AccountDto savedAccount = new AccountDto(
+                1L,
+                "12345678",
+                "David",
+                1L,
+                AccountType.CHECKING,
+                BigDecimal.valueOf(2000.0),
+                true
+        );
+        when(accountService.create(newAccount)).thenReturn(savedAccount);
+
+        ResponseEntity<AccountDto> response = accountController.create(newAccount);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(savedAccount, response.getBody());
+    }
+
+    @Test
+    void getAccountByIdTest() {
+        Long accountId = 1L;
+        AccountDto account = new AccountDto(
+                1L,
+                "12345678",
+                "David",
+                1L,
+                AccountType.CHECKING,
+                BigDecimal.valueOf(2000.0),
+                true
+        );
+
+        when(accountService.getById(accountId)).thenReturn(account);
+        ResponseEntity<AccountDto> response = accountController.getById(accountId);
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(account, response.getBody());
+    }
 }
--- client/src/test/java/com/app/client/ClientApplicationTests.java
+++ client/src/test/java/com/app/client/ClientApplicationTests.java
@@ -1,13 +1,116 @@
 package com.app.client;
 
+import com.app.client.controller.ClientController;
+import com.app.client.model.dto.ClientDto;
+import com.app.client.model.dto.ClientUpdateDto;
+import com.app.client.service.ClientService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class ClientApplicationTests {
+    private final ClientService clientService = mock(ClientService.class);
+    private final ClientController clientController = new ClientController(clientService);
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @Test
+    void saveClientTest() {
+        ClientDto newClient = new ClientDto(
+                1L,
+                "David",
+                "Male",
+                (short) 27,
+                "099999999",
+                "Quito",
+                "099999999",
+                "password",
+                true
+        );
+        ClientDto savedClient = new ClientDto(
+                1L,
+                "David",
+                "Male",
+                (short) 27,
+                "099999999",
+                "Quito",
+                "099999999",
+                "password",
+                true
+        );
+        when(clientService.create(newClient)).thenReturn(savedClient);
+
+        ResponseEntity<ClientDto> response = clientController.create(newClient);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(savedClient, response.getBody());
+    }
+
+    @Test
+    void updateClientTest() {
+        Long clientId = 1L;
+
+        ClientUpdateDto updateDto = new ClientUpdateDto(
+                "David Updated",
+                "Male",
+                (short) 28,
+                "088888888",
+                "Guayaquil",
+                "088888888",
+                "newpassword",
+                true
+        );
+
+        ClientDto updatedClient = new ClientDto(
+                clientId,
+                "David Updated",
+                "Male",
+                (short) 28,
+                "088888888",
+                "Guayaquil",
+                "088888888",
+                "newpassword",
+                true
+        );
+
+        when(clientService.update(clientId, updateDto)).thenReturn(updatedClient);
+
+        ResponseEntity<ClientDto> response = clientController.update(clientId, updateDto);
+
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(updatedClient, response.getBody());
+    }
+
+    @Test
+    void getClientByIdTest() {
+        Long clientId = 1L;
+        ClientDto client = new ClientDto(
+                1L,
+                "David",
+                "Male",
+                (short) 27,
+                "099999999",
+                "Quito",
+                "099999999",
+                "password",
+                true
+        );
+
+        when(clientService.getById(clientId)).thenReturn(client);
+        ResponseEntity<ClientDto> response = clientController.getById(clientId);
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(client, response.getBody());
+    }
 }
