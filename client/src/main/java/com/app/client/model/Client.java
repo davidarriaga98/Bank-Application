7e90229e8ae28bb9043aefc0c85a16ad24e614f5
David Arriaga <david.arriaga@formos.com>
2026-04-23 09:42:29 -0500

    Add basic struct in client microservice

---
+++ client/src/main/java/com/app/client/model/Client.java
@@ -0,0 +1,4 @@
+package com.app.client.model;
+
+public class Client {
+}
---
+++ client/src/main/java/com/app/client/service/Client.java
@@ -0,0 +1,8 @@
+package com.app.client.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+class Client {
+
+}
